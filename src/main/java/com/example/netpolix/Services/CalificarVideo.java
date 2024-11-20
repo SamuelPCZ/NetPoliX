56b0ec5e89bdb526b46863ceea9463088a7618f6
adavgonzalez <andresgonzalezcuesta432@gmail.com>
2024-11-20 07:42:03 -0500

    estilos agregados, falta corregir la navegacion del sistema

--- src/main/java/com/example/netpolix/Services/CalificarVideo.java
+++ src/main/java/com/example/netpolix/Services/CalificarVideo.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.netpolix.Repository.CalificacionesRepository;
+import com.example.netpolix.Repository.UserRepository;
 import com.example.netpolix.Repository.VideoRepository;
 import com.example.netpolix.model.Calificaciones;
+import com.example.netpolix.model.Usuario;
 import com.example.netpolix.model.Video;
 
 import java.util.List;
@@ -19,6 +21,9 @@ public class CalificarVideo {
     @Autowired
     private CalificacionesRepository calificacionesRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public void calificarVideo(int isan, int idUsuario, float calificacion) {
         Video video = videoRepository.findByIsan(isan);
         if (video != null) {
@@ -30,6 +35,11 @@ public class CalificarVideo {
                 nuevaCalificacion.setIdUsuario(idUsuario);
                 nuevaCalificacion.setCalificacion(calificacion);
                 calificacionesRepository.save(nuevaCalificacion);
+
+                // Actualizar los puntos del usuario
+                Usuario usuario = userRepository.findById((long) idUsuario).orElseThrow(() -> new IllegalArgumentException("Usuario no encontrado"));
+                usuario.setPuntos(usuario.getPuntos() + 20);
+                userRepository.save(usuario);
             } else {
                 throw new IllegalArgumentException("El usuario ya ha calificado este video.");
             }
